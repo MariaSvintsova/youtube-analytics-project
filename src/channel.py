60692dedac9f3b66ce9b4ad3904e347d4921372b
MariaSvintsova <m.svintsova@it-park.tech>
2023-09-29 14:03:08 +0300

    second hw

--- src/channel.py
+++ src/channel.py
@@ -10,6 +10,12 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.title = ''
+        self.description = ''
+        self.url = ''
+        self.subscriber_count = 0
+        self.video_count = 0
+        self.view_count = 0
 
     def print_info(self):
         """Выводит в консоль информацию о канале в json формате."""
@@ -20,6 +26,30 @@ class Channel:
         channel_id = self.channel_id
         channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API."""
+
+        api_key = "AIzaSyCg8I_OJBrkfqsq_KihzECdLwGtRhbNMLU"
+        youtube_service = build("youtube", 'v3', developerKey=api_key)
+        return youtube_service
+    def to_json(self, filename):
+        channel_data = {
+            "channel_id":self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscriber_count": self.subscriber_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+
+        with open(filename, "w") as json_file:
+            json.dump(channel_data, json_file, indent=4)
+
+
+
+
 
 
 """Создается экземпляр класса, вызывается его метод, выводящий всю информацию."""
